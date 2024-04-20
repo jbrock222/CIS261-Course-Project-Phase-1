20d11ba6b9bbcd563323a0a81cf168c3adc4e2ea
username <useremail>
2024-04-20 12:17:01 -0700

    Initial commit

--- CIS261_Course_Project.py
+++ CIS261_Course_Project.py
@@ -4,3 +4,33 @@ CIS261
 Course Project Phase 1
 '''
 
+index = 0
+employeeName = " "
+totalHours = 0
+hourlyRate = 0
+taxRate = 0
+
+def enterName():
+    global employeeName
+    print("enter employee name")
+
+def enterHours():
+    global totalHours
+    print("enter total hours")
+
+def enterHourlyRate():
+    global hourlyRate
+    print("enter hourly rate")
+    
+def enterTaxRate():
+    global taxRate
+    print("enter tax rate")
+    
+def calculate():
+    print("calculate")
+    
+functions = [enterName, enterHours, enterHourlyRate, enterTaxRate, calculate]
+
+for f in functions:
+    result = f()
+
